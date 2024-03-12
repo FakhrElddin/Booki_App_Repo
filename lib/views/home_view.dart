291d27f58b51bf9112f78fef9b3b38bc91325f17
FakhrElddin <fa1641@fayoum.edu.eg>
2024-03-12 07:15:26 +0200

    create grid view item and categories list view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:graduation_project/widgets/book_item.dart';
+import 'package:graduation_project/widgets/grid_view_item.dart';
+import 'package:graduation_project/widgets/test.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -15,42 +17,53 @@ class HomeView extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Text(
-              'Last Seen',
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            SizedBox(
-              height: 260,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                physics: const BouncingScrollPhysics(),
-                itemBuilder: (context, index) => const BookItme(),
-                separatorBuilder: (context, index) => const SizedBox(width: 10,),
-                itemCount: 10,
-              ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            const Text(
               'For You',
             ),
             const SizedBox(
               height: 8,
             ),
             SizedBox(
-              height: 260,
+              height: 250,
               child: ListView.separated(
                 scrollDirection: Axis.horizontal,
                 physics: const BouncingScrollPhysics(),
-                itemBuilder: (context, index) => const BookItme(),
-                separatorBuilder: (context, index) => const SizedBox(width: 10,),
+                itemBuilder: (context, index) => const BookItem(),
+                separatorBuilder: (context, index) => const SizedBox(width: 24,),
                 itemCount: 10,
               ),
             ),
             const SizedBox(
-              height: 16,
+              height: 32,
+            ),
+            SizedBox(
+              height: 32,
+              child: ListView.separated(
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) => OutlinedButton(
+                  onPressed: (){},
+                  child: const Text(
+                    'General',
+                  ),
+                ),
+                separatorBuilder: (context, index) => SizedBox(width: 16,),
+                itemCount: 10,
+              ),
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: .49,
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+              ),
+              itemBuilder: (context, index) => const GridViewItem(),
+              itemCount: 20,
             ),
           ],
         ),
