065799a41a1f2b42d46c3bd6f70af628c03628dc
FakhrElddin <fa1641@fayoum.edu.eg>
2024-03-12 15:51:46 +0200

    fix bottom nav bar padding

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graduation_project/constants.dart';
+import 'package:graduation_project/cubits/app_cubit/app_cubit.dart';
 import 'package:graduation_project/cubits/bottom_nav_bar_cubit/bottom_nav_bar_cubit.dart';
 import 'package:graduation_project/helper/dio_helper.dart';
 import 'package:graduation_project/local/cache_helper.dart';
 import 'package:graduation_project/simple_bloc_observer.dart';
 import 'package:graduation_project/themes/light_theme.dart';
 import 'package:graduation_project/views/app_view.dart';
-import 'package:graduation_project/views/forgot_password_view.dart';
 import 'package:graduation_project/views/login_view.dart';
 import 'package:graduation_project/views/on_boarding_view.dart';
-import 'package:graduation_project/views/reset_password_view.dart';
-import 'package:graduation_project/views/verify_code_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -41,6 +39,7 @@ class BookiApp extends StatelessWidget {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) => BottomNavBarCubit(),),
+        BlocProvider(create: (context) => AppCubit(),),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,73 +1,67 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:graduation_project/cubits/app_cubit/app_cubit.dart';
 import 'package:graduation_project/widgets/book_item.dart';
 import 'package:graduation_project/widgets/grid_view_item.dart';
-import 'package:graduation_project/widgets/test.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'For You',
-            ),
-            const SizedBox(
-              height: 8,
-            ),
-            SizedBox(
-              height: 250,
-              child: ListView.separated(
-                scrollDirection: Axis.horizontal,
-                physics: const BouncingScrollPhysics(),
-                itemBuilder: (context, index) => const BookItem(),
-                separatorBuilder: (context, index) => const SizedBox(width: 24,),
-                itemCount: 10,
-              ),
-            ),
-            const SizedBox(
-              height: 32,
-            ),
-            SizedBox(
-              height: 32,
-              child: ListView.separated(
-                physics: const BouncingScrollPhysics(),
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) => OutlinedButton(
-                  onPressed: (){},
-                  child: const Text(
-                    'General',
+    return BlocConsumer<AppCubit, AppState>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      builder: (context, state) {
+        var appCubit = BlocProvider.of<AppCubit>(context);
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 15,
+                ),
+                const Text(
+                  'For You',
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                SizedBox(
+                  height: 250,
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    physics: const BouncingScrollPhysics(),
+                    itemBuilder: (context, index) => const BookItem(),
+                    separatorBuilder: (context, index) =>
+                    const SizedBox(width: 24,),
+                    itemCount: 10,
                   ),
                 ),
-                separatorBuilder: (context, index) => SizedBox(width: 16,),
-                itemCount: 10,
-              ),
+                const SizedBox(
+                  height: 64,
+                ),
+                GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: .49,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                  ),
+                  itemBuilder: (context, index) => const GridViewItem(),
+                  itemCount: 20,
+                ),
+              ],
             ),
-            const SizedBox(
-              height: 32,
-            ),
-            GridView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                childAspectRatio: .49,
-                crossAxisSpacing: 16,
-                mainAxisSpacing: 16,
-              ),
-              itemBuilder: (context, index) => const GridViewItem(),
-              itemCount: 20,
-            ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
