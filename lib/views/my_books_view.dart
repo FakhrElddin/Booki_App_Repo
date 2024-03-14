e0e00813ed272d04e2d81cb552c090f39b696ee2
FakhrElddin <fa1641@fayoum.edu.eg>
2024-03-14 16:42:24 +0200

    create my books item and my books view

--- lib/views/my_books_view.dart
+++ lib/views/my_books_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:graduation_project/widgets/book_item.dart';
+import 'package:graduation_project/widgets/my_books_item.dart';
 
 class MyBooksView extends StatelessWidget {
   const MyBooksView({super.key});
@@ -17,7 +19,36 @@ class MyBooksView extends StatelessWidget {
           ),
         ),
       ),
-      body: Column(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 16,
+              ),
+              const Text(
+                'My Books',
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              ListView.separated(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (context, index) => const MyBooksItem(),
+                separatorBuilder: (context, index) => const SizedBox(height: 32,),
+                itemCount: 10,
+              ),
+              const SizedBox(
+                height: 100,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
