633853c33def4050fd7dc35122da4af25c5034fc
mo'men <momenali5792@gmail.com>
2024-03-14 03:48:00 +0200

    initial code without refactor for book page

--- lib/views/book_view.dart
+++ lib/views/book_view.dart
@@ -34,12 +34,14 @@ class BookView extends StatelessWidget {
             ),
             Center(
               child: Container(
-                height: 300,
-                width: 180,
+                height: 275,
+                width: 185,
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
+                  borderRadius: BorderRadius.circular(10),
                   image: DecorationImage(
-                    image:AssetImage('assets/images/ibn.jpg'),
+                    fit: BoxFit.fill,
+                    image:AssetImage('assets/images/ibn.jpg',
+                    ),
                   ),
                 ),
                 ),
@@ -52,6 +54,163 @@ class BookView extends StatelessWidget {
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
               ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Icon(Icons.circle,
+                  size: 7,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("Edition : 1999",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Icon(Icons.circle,
+                    size: 7,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("Category : Religious",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Icon(Icons.circle,
+                    size: 7,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("Time used : 2 years",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Icon(Icons.circle,
+                    size: 7,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("Price : 20 L.E",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Icon(Icons.circle,
+                    size: 7,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text("Book status : Good",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Divider(
+              height: 10,
+              thickness: 2.35,
+              color: Colors.grey,
+              indent: 50,
+              endIndent: 50,
+            ),
+            SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  style:ElevatedButton.styleFrom(
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ) ,
+                    backgroundColor: Color(0xff0766AD),
+                    fixedSize: Size(220, 60),
+                  ),
+                  onPressed: (){}, child: Text('Order',
+                  ),
+                  ),
+
+                ElevatedButton(
+                  style:ElevatedButton.styleFrom(
+                    textStyle: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ) ,
+                    backgroundColor: Color(0xffE2E7F0),
+                      fixedSize: Size(140, 60),
+                    foregroundColor: Colors.black,
+                  ),
+                  onPressed: (){}, child: Text('Exchange',
+                ),
+                ),
+              ],
             ),
           ],
         ),
