df076a600216b062b66b8e715546a17d519eafc1
mo'men <momenali5792@gmail.com>
2024-03-20 01:09:14 +0200

    edited stars output

--- lib/widgets/star_display_item.dart
+++ lib/widgets/star_display_item.dart
@@ -5,15 +5,20 @@ class StarDisplay extends StatelessWidget {
    const StarDisplay({ Key? key, this.value = 0, required this.size})
       : super(key: key);
   @override
+  @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisSize: MainAxisSize.min,
       children: List.generate(5, (index) {
-        return Icon(
-          index < value ? Icons.star : Icons.star_border,
-          size: size,
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 2.0),
+          child: Icon(
+            index < value ? Icons.star : Icons.star_border,
+            size: size,
+          ),
         );
       }),
     );
   }
+
 }
