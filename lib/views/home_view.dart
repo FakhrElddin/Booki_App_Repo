bcaae234edb5379a599ae806d6e08d1072ca5a2d
FakhrElddin <fa1641@fayoum.edu.eg>
2024-04-12 18:15:13 +0200

    add category buttons to home screen

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,11 +1,22 @@
+import 'package:custom_radio_grouped_button/custom_radio_grouped_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:graduation_project/constants.dart';
 import 'package:graduation_project/cubits/app_cubit/app_cubit.dart';
 import 'package:graduation_project/widgets/book_item.dart';
 import 'package:graduation_project/widgets/grid_view_item.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
+
+  final List<String> buttons = const [
+    'General',
+    'Sports',
+    'Business',
+    'Science',
+    'Fiction',
+    'Historical',
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +54,36 @@ class HomeView extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(
-                  height: 64,
+                  height: 40,
+                ),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  physics: const BouncingScrollPhysics(),
+                  child: CustomRadioButton(
+                    margin: const EdgeInsetsDirectional.only(end: 16),
+                    elevation: 0,
+                    height: 40,
+                    shapeRadius: 5,
+                    autoWidth: true,
+                    enableShape: true,
+                    selectedBorderColor: kPrimaryColor,
+                    unSelectedBorderColor: kPrimaryColor,
+                    unSelectedColor: Colors.white,
+                    buttonLables: buttons,
+                    buttonValues: buttons,
+                    defaultSelected: buttons[0],
+                    buttonTextStyle: const ButtonTextStyle(
+                        selectedColor: Colors.white,
+                        unSelectedColor: kPrimaryColor,
+                        textStyle: TextStyle(fontSize: 16)),
+                    radioButtonValue: (value) {
+                      print(value);
+                    },
+                    selectedColor: kPrimaryColor,
+                  ),
+                ),
+                const SizedBox(
+                  height: 40,
                 ),
                 GridView.builder(
                   shrinkWrap: true,
