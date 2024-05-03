dd410cc50abe50ac43998fdef69bc547b1c24998
mo'men <momenali5792@gmail.com>
2024-05-03 18:19:11 +0300

    Added region book owner full information view

--- lib/views/book_owner_full_profile_view.dart
+++ lib/views/book_owner_full_profile_view.dart
@@ -148,6 +148,40 @@ class BookOwnerFullProfileView extends StatelessWidget {
                 height:
                 20,
               ),
+              const Text("City/region",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(
+                height:
+                10,
+              ),
+              Container(
+                height: 60,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                    ),
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(6)
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(16),
+                  child: Text("Cairo",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height:
+                20,
+              ),
               const Text('Address',
                 style: TextStyle(
                   fontSize: 20,
