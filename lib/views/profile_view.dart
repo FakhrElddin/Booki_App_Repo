e2f8e755fdf3c9b9283a873a27ec104c081f7b19
mo'men <momenali5792@gmail.com>
2024-06-18 22:27:16 +0300

    added user card id in multiple pages

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -1,177 +1,311 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:graduation_project/models/user_model.dart';
+import 'package:graduation_project/widgets/user_service.dart';
 import 'package:graduation_project/local/cache_helper.dart';
-import 'package:graduation_project/views/update_user_info.dart';
+import 'login_view.dart';
+import 'update_user_info.dart';
 import '../constants.dart';
 import '../widgets/book_owner_full_profile_body.dart';
-import '../widgets/book_owner_info.dart';
-import 'login_view.dart';
 
-class ProfileView extends StatelessWidget {
-  const ProfileView({super.key});
+class ProfileView extends StatefulWidget {
+  const ProfileView({Key? key});
+
+  @override
+  State<ProfileView> createState() => _ProfileViewState();
+}
+
+class _ProfileViewState extends State<ProfileView> {
+  late Future<UserModel> futureUser;
+
+  @override
+  void initState() {
+    super.initState();
+    futureUser = UserService(Dio()).getUserInfo();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                BookOwnerInfo(),
-                BookOwnerFullProfileBody(),
-                const SizedBox(
-                  height: 24,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 25),
+    return FutureBuilder<UserModel>(
+      future: futureUser,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        // } else if (snapshot.hasError) {
+        //   return const Center(child: Text('Failed to load user data'));
+        } else if (snapshot.hasData) {
+          final user = snapshot.data!;
+          return Column(
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          textStyle: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                          ),
-                          backgroundColor: kPrimaryColor,
-                          fixedSize: const Size(360, 60),
-                        ),
-                        onPressed: () {
-                          Navigator.push(context,
-                              MaterialPageRoute(builder: (context) {
-                                return const UpdateUserInfo();
-                              }));
-                        },
-                        child: const Text(
-                          'Update profile',
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 24,
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          textStyle: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                          ),
-                          backgroundColor: const Color(0xffE2E7F0),
-                          fixedSize: const Size(360, 60),
-                          foregroundColor: Colors.black,
-                        ),
-                        onPressed: () {
-                          CacheHelper.removeData(key: 'token');
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return const LoginView();
+                      BookOwnerFullProfileBody(),
+                      const SizedBox(height: 24),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 25),
+                        child: Column(
+                          children: [
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                textStyle: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                backgroundColor: kPrimaryColor,
+                                fixedSize: const Size(360, 60),
+                              ),
+                              onPressed: () {
+                                Navigator.push(context,
+                                    MaterialPageRoute(builder: (context) {
+                                      return const UpdateUserInfo();
+                                    }));
                               },
+                              child: const Text('Update profile'),
                             ),
-                          );
-                        },
-                        child: const Text(
-                          'log out',
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 24,
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          textStyle: const TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                          ),
-                          backgroundColor:  Colors.red,
-                          fixedSize: const Size(360, 60),
-                          foregroundColor: Colors.black,
-                        ),
-                        onPressed: () {
-                          CacheHelper.removeData(key: 'token');
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) {
-                                return const LoginView();
+                            const SizedBox(height: 24),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                textStyle: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                backgroundColor: const Color(0xffE2E7F0),
+                                fixedSize: const Size(360, 60),
+                                foregroundColor: Colors.black,
+                              ),
+                              onPressed: () {
+                                CacheHelper.removeData(key: 'token');
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) {
+                                      return const LoginView();
+                                    },
+                                  ),
+                                );
                               },
+                              child: const Text('Log out'),
                             ),
-                          );
-                        },
-                        child: const Text(
-                          'Delete account',
+                            const SizedBox(height: 24),
+                            ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                textStyle: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                backgroundColor: Colors.red,
+                                fixedSize: const Size(360, 60),
+                                foregroundColor: Colors.black,
+                              ),
+                              onPressed: () {
+                                CacheHelper.removeData(key: 'token');
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) {
+                                      return const LoginView();
+                                    },
+                                  ),
+                                );
+                              },
+                              child: const Text('Delete account'),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   ),
                 ),
-
-              ],
-            ),
-          ),
-        ),
-        // Padding(
-        //   padding: const EdgeInsets.only(
-        //     left: 16,
-        //     right: 16,
-        //     bottom: 20,
-        //   ),
-        //   child: Row(
-        //     children: [
-        //       Expanded(
-        //         flex: 2,
-        //         child: ElevatedButton(
-        //           style: ElevatedButton.styleFrom(
-        //             textStyle: const TextStyle(
-        //               fontSize: 20,
-        //               fontWeight: FontWeight.w500,
-        //             ),
-        //             backgroundColor: kPrimaryColor,
-        //             fixedSize: const Size(220, 60),
-        //           ),
-        //           onPressed: () {
-        //             Navigator.push(context,
-        //                 MaterialPageRoute(builder: (context) {
-        //                  return const UpdateUserInfo();
-        //                 }));
-        //           },
-        //           child: const Text(
-        //             'Update profile',
-        //           ),
-        //         ),
-        //       ),
-        //       const SizedBox(
-        //         width: 12,
-        //       ),
-        //       Expanded(
-        //         flex: 1,
-        //         child: ElevatedButton(
-        //           style: ElevatedButton.styleFrom(
-        //             textStyle: const TextStyle(
-        //               fontSize: 20,
-        //               fontWeight: FontWeight.w500,
-        //             ),
-        //             backgroundColor: const Color(0xffE2E7F0),
-        //             fixedSize: const Size(140, 60),
-        //             foregroundColor: Colors.black,
-        //           ),
-        //           onPressed: () {
-        //             CacheHelper.removeData(key: 'token');
-        //             Navigator.pushReplacement(context, MaterialPageRoute(builder: (context){
-        //               return const LoginView();
-        //             },),);
-        //           },
-        //           child: const Text(
-        //             'log out',
-        //           ),
-        //         ),
-        //       ),
-        //     ],
-        //   ),
-        // ),
-      ],
+              ),
+            ],
+          );
+        }
+        else {
+          return const Center(child: Text('No user data available'));
+        }
+      },
     );
   }
 }
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:graduation_project/local/cache_helper.dart';
+// import 'package:graduation_project/views/update_user_info.dart';
+// import '../constants.dart';
+// import '../widgets/book_owner_full_profile_body.dart';
+// import 'login_view.dart';
+//
+// class ProfileView extends StatelessWidget {
+//   const ProfileView({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         Expanded(
+//           child: SingleChildScrollView(
+//             physics: BouncingScrollPhysics(),
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.start,
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 BookOwnerFullProfileBody(),
+//                 const SizedBox(
+//                   height: 24,
+//                 ),
+//                 Padding(
+//                   padding: const EdgeInsets.only(left: 25),
+//                   child: Column(
+//                     children: [
+//                       ElevatedButton(
+//                         style: ElevatedButton.styleFrom(
+//                           textStyle: const TextStyle(
+//                             fontSize: 20,
+//                             fontWeight: FontWeight.w500,
+//                           ),
+//                           backgroundColor: kPrimaryColor,
+//                           fixedSize: const Size(360, 60),
+//                         ),
+//                         onPressed: () {
+//                           Navigator.push(context,
+//                               MaterialPageRoute(builder: (context) {
+//                             return const UpdateUserInfo();
+//                           }));
+//                         },
+//                         child: const Text(
+//                           'Update profile',
+//                         ),
+//                       ),
+//                       const SizedBox(
+//                         height: 24,
+//                       ),
+//                       ElevatedButton(
+//                         style: ElevatedButton.styleFrom(
+//                           textStyle: const TextStyle(
+//                             fontSize: 20,
+//                             fontWeight: FontWeight.w500,
+//                           ),
+//                           backgroundColor: const Color(0xffE2E7F0),
+//                           fixedSize: const Size(360, 60),
+//                           foregroundColor: Colors.black,
+//                         ),
+//                         onPressed: () {
+//                           CacheHelper.removeData(key: 'token');
+//                           Navigator.pushReplacement(
+//                             context,
+//                             MaterialPageRoute(
+//                               builder: (context) {
+//                                 return const LoginView();
+//                               },
+//                             ),
+//                           );
+//                         },
+//                         child: const Text(
+//                           'log out',
+//                         ),
+//                       ),
+//                       const SizedBox(
+//                         height: 24,
+//                       ),
+//                       ElevatedButton(
+//                         style: ElevatedButton.styleFrom(
+//                           textStyle: const TextStyle(
+//                             fontSize: 20,
+//                             fontWeight: FontWeight.w500,
+//                           ),
+//                           backgroundColor: Colors.red,
+//                           fixedSize: const Size(360, 60),
+//                           foregroundColor: Colors.black,
+//                         ),
+//                         onPressed: () {
+//                           CacheHelper.removeData(key: 'token');
+//                           Navigator.pushReplacement(
+//                             context,
+//                             MaterialPageRoute(
+//                               builder: (context) {
+//                                 return const LoginView();
+//                               },
+//                             ),
+//                           );
+//                         },
+//                         child: const Text(
+//                           'Delete account',
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ),
+//       ],
+//     );
+//   }
+// }
+// Padding(
+//   padding: const EdgeInsets.only(
+//     left: 16,
+//     right: 16,
+//     bottom: 20,
+//   ),
+//   child: Row(
+//     children: [
+//       Expanded(
+//         flex: 2,
+//         child: ElevatedButton(
+//           style: ElevatedButton.styleFrom(
+//             textStyle: const TextStyle(
+//               fontSize: 20,
+//               fontWeight: FontWeight.w500,
+//             ),
+//             backgroundColor: kPrimaryColor,
+//             fixedSize: const Size(220, 60),
+//           ),
+//           onPressed: () {
+//             Navigator.push(context,
+//                 MaterialPageRoute(builder: (context) {
+//                  return const UpdateUserInfo();
+//                 }));
+//           },
+//           child: const Text(
+//             'Update profile',
+//           ),
+//         ),
+//       ),
+//       const SizedBox(
+//         width: 12,
+//       ),
+//       Expanded(
+//         flex: 1,
+//         child: ElevatedButton(
+//           style: ElevatedButton.styleFrom(
+//             textStyle: const TextStyle(
+//               fontSize: 20,
+//               fontWeight: FontWeight.w500,
+//             ),
+//             backgroundColor: const Color(0xffE2E7F0),
+//             fixedSize: const Size(140, 60),
+//             foregroundColor: Colors.black,
+//           ),
+//           onPressed: () {
+//             CacheHelper.removeData(key: 'token');
+//             Navigator.pushReplacement(context, MaterialPageRoute(builder: (context){
+//               return const LoginView();
+//             },),);
+//           },
+//           child: const Text(
+//             'log out',
+//           ),
+//         ),
+//       ),
+//     ],
+//   ),
+// ),
---
+++ lib/widgets/user_service.dart
@@ -0,0 +1,27 @@
+// services/user_service.dart
+
+import 'package:dio/dio.dart';
+import '../constants.dart';
+import '../models/user_model.dart';
+
+class UserService {
+  final Dio dio;
+
+  UserService(this.dio);
+
+  Future<UserModel> getUserInfo() async {
+    try {
+      dio.options.headers={"Authorization":"Bearer $token"};
+      final response =
+      await dio.get('http://10.0.2.2:4000/api/v1/user/getMyInfo');
+      final jsonData = response.data['data'];
+      print(jsonData);
+      return UserModel.fromJson(jsonData);
+    } on DioException catch(e){
+      print(e.response.toString());
+      throw Exception('Failed to load user info 1');
+    }catch (e) {
+      throw Exception('Failed to load user info');
+    }
+  }
+}
