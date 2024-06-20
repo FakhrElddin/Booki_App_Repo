458d7cb1e70d27b87231b34560fcdc210f5a4771
mo'men <momenali5792@gmail.com>
2024-06-20 18:41:59 +0300

    added a few edits

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:graduation_project/models/user_model.dart';
 import 'package:graduation_project/widgets/user_service.dart';
-import 'package:graduation_project/local/cache_helper.dart';
-import 'login_view.dart';
-import 'update_user_info.dart';
-import '../constants.dart';
 import '../widgets/user_full_profile_body.dart';
 
 class ProfileView extends StatefulWidget {
@@ -45,79 +41,6 @@ class _ProfileViewState extends State<ProfileView> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       UserProfileBody(user: user,),
-                      const SizedBox(height: 24),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 25),
-                        child: Column(
-                          children: [
-                            ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                textStyle: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                                backgroundColor: kPrimaryColor,
-                                fixedSize: const Size(360, 60),
-                              ),
-                              onPressed: () {
-                                Navigator.push(context,
-                                    MaterialPageRoute(builder: (context) {
-                                      return const UpdateUserInfo();
-                                    }));
-                              },
-                              child: const Text('Update profile'),
-                            ),
-                            const SizedBox(height: 24),
-                            ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                textStyle: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                                backgroundColor: const Color(0xffE2E7F0),
-                                fixedSize: const Size(360, 60),
-                                foregroundColor: Colors.black,
-                              ),
-                              onPressed: () {
-                                CacheHelper.removeData(key: 'token');
-                                Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) {
-                                      return const LoginView();
-                                    },
-                                  ),
-                                );
-                              },
-                              child: const Text('Log out'),
-                            ),
-                            const SizedBox(height: 24),
-                            ElevatedButton(
-                              style: ElevatedButton.styleFrom(
-                                textStyle: const TextStyle(
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                                backgroundColor: Colors.red,
-                                fixedSize: const Size(360, 60),
-                                foregroundColor: Colors.black,
-                              ),
-                              onPressed: () {
-                                CacheHelper.removeData(key: 'token');
-                                Navigator.pushReplacement(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) {
-                                      return const LoginView();
-                                    },
-                                  ),
-                                );
-                              },
-                              child: const Text('Delete account'),
-                            ),
-                          ],
-                        ),
-                      ),
                     ],
                   ),
                 ),
--- lib/widgets/user_full_profile_body.dart
+++ lib/widgets/user_full_profile_body.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:graduation_project/models/user_model.dart';
 import 'package:graduation_project/widgets/star_display_item.dart';
 
+import '../constants.dart';
+import '../local/cache_helper.dart';
+import '../views/login_view.dart';
+import '../views/update_user_info.dart';
+
 class UserProfileBody extends StatelessWidget {
   const UserProfileBody({super.key, required this.user});
   final UserModel user;
@@ -186,7 +191,76 @@ class UserProfileBody extends StatelessWidget {
               ),
             ),
             const SizedBox(
-              height: 20,
+              height: 30,
+            ),
+            Column(
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    backgroundColor: kPrimaryColor,
+                    fixedSize: const Size(360, 60),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                          return const UpdateUserInfo();
+                        }));
+                  },
+                  child: const Text('Update profile'),
+                ),
+                const SizedBox(height: 24),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    backgroundColor: const Color(0xffE2E7F0),
+                    fixedSize: const Size(360, 60),
+                    foregroundColor: Colors.black,
+                  ),
+                  onPressed: () {
+                    CacheHelper.removeData(key: 'token');
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return const LoginView();
+                        },
+                      ),
+                    );
+                  },
+                  child: const Text('Log out'),
+                ),
+                const SizedBox(height: 24),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    textStyle: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                    backgroundColor: Colors.red,
+                    fixedSize: const Size(360, 60),
+                    foregroundColor: Colors.black,
+                  ),
+                  onPressed: () {
+                    CacheHelper.removeData(key: 'token');
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return const LoginView();
+                        },
+                      ),
+                    );
+                  },
+                  child: const Text('Delete account'),
+                ),
+              ],
             ),
           ],
         ),
